072b24fa1c3aedf3837142be9af8056b398f0e8a
jinwoo <goodtim123@naver.com>
2022-11-25 09:16:42 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :artists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	get 'albums/:album_id/show_albums', to: 'albums#show', as: :album_show
-	root 'artists#index'
+	
+	root 'home#index'
 end
